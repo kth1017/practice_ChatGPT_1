0b8159ada8d11a39d2f75bdd1492890b64592678
kth <rnjsxo1017@gmail.com>
2023-02-02 23:57:02 +0900

    start

---
+++ src/main/java/fadet/GptTest/GptTestApplication.java
@@ -0,0 +1,13 @@
+package fadet.GptTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptTestApplication.class, args);
+	}
+
+}
