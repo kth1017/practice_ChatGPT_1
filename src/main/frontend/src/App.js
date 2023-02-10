0b5cc7bcb3e683520048e42ed26e91fff0a3cf6e
kth <rnjsxo1017@gmail.com>
2023-02-10 20:35:05 +0900

    필드 바인딩 성공

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,10 +2,13 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+
+
 function Form(props) {
-    const [newinputQ, setNewinputQ] = useState("null");
+    const [newinputQ, setNewinputQ] = useState(props.Q);
 
     return <form onSubmit={event =>{
+        event.preventDefault();
         const originQ = event.target.originQ.value;
         const originA = event.target.originA.value;
         props.onForm(originQ, originA);
@@ -23,20 +26,27 @@ function Form(props) {
 
 function App() {
     const [hello, setHello] = useState('');
+    const [stateq, setStateQ] = useState('init');
+    var q = "";
+    var saveQ = '';
+
+    console.log("시작 q " + q);
+    console.log("시작 saveQ " + saveQ);  
+    console.log("시작 stateQ " + stateq);  
 
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => setHello(response.data))
         .catch(error => console.log(error))
-    }, []);
-
+    }, [stateq]);
 
     return (
 
         <div>
-            <Form onForm={(_originQ, _originA) => {
+            <Form Q={stateq} onForm={(_originQ, _originA) => {
                 // console.log("2nd = " + _title);
                 // setTitle(_title); < 이거 작동안함
+
                 axios.post('/request', 
                     {originQ: `${_originQ}`, originA: `${_originA}`}
                   )
@@ -46,7 +56,12 @@ function App() {
                   })
                   .catch(function (error) {
                     console.log(error);
-                  })
+                  });
+                saveQ = _originQ; 
+                console.log(saveQ);
+                setStateQ(saveQ);              
+
+
             }}></Form>
             
                         백엔드에서 가져온 데이터입니다 : {hello}
