6cbeba7dc5e8c9de08b24882f9e2ddf688306407
kth <rnjsxo1017@gmail.com>
2023-02-14 02:00:20 +0900

    중간 완료

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,6 +5,9 @@ import { Button, ButtonGroup } from '@mui/material'
 import { Input, TextField } from '@mui/material';
 import { Container, Grid } from '@mui/material';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { Typography, Icon } from '@mui/material';
+import '@fontsource/roboto/700.css';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 
 
 function Form(props) {
@@ -34,60 +37,61 @@ function Form(props) {
             
 }
 
-function CustomDial(props){
-    const [mod, setMod] = useState(props.Mod);
-    return <Dialog open={mod}>
-    <DialogTitle>사용법</DialogTitle>
-    <DialogContent> 
-        <DialogContentText>
-            1. ai에게 할 질문이 한글이라면 번역을 위한 한글 질문을 입력해주시거나 추천 질문 버튼을 눌러주세요.
-            2. 번역된 질문 또는 직접 입력한 영어 질문이 'ai에게 질문하기' 버튼을 누르시면 아래에 답변이 출력됩니다.
-        </DialogContentText>
-        <DialogActions>
-            <Button variant='contained' onClick={(event) => console.log(event)}>닫기</Button>
-        </DialogActions>
-    </DialogContent>
-</Dialog>
-}
-
 function App() {
     const [test, setTest] = useState([]);
-    const [bindingQ, setBindingQ] = useState('bindingQ'); 
+    const [bindingQ, setBindingQ] = useState(null); 
     const [transQ, setTransQ] = useState(null);
     const [resultA, SetResultA] = useState(null);
     const [dialMod, setDialMod] = useState(false);
 
+
+    useEffect(() => {
+        console.log("시작시 tq = "+transQ);
+        axios.get('/apiTest')
+        .then(response => {setTest(response.data)})
+        .catch(error => console.log(error))
+        
+    }, []);
     
     useEffect(() => {
         axios.get('/api/transQ')
         .then(response => setTransQ(JSON.stringify(response.data.message.result.translatedText).replace(/"/gi, "")))
         .catch(error => console.log(error))
+        console.log("번역 get tq = "+transQ);
+        console.log("번역 get bq = "+bindingQ);
     }, [bindingQ]);
 
-    useEffect(() => {
-        axios.get('/apiTest')
-        .then(response => {setTest(response.data)})
-        .catch(error => console.log(error))
-    }, []);
+
     
-    useEffect(() => {
-        axios.get('/api/sendQ')
-        .then(response => SetResultA(JSON.stringify(response.data.choices[0].text).replace(/"/gi, "")))
-        .catch(error => console.log(error)) 
-    }, [resultA]);
+    
 
     return (
         <>
-        <CustomDial Mod={dialMod}></CustomDial>
-        <span>Gpt api 웹사이트</span><br/>  <br/>  
+        <Container>
+        <Dialog open={dialMod}>
+            <DialogTitle>사용법</DialogTitle>
+                <DialogContent> 
+                    <DialogContentText>
+                        1. ai에게 할 질문이 한글이라면 번역을 위한 한글 질문을 입력해주시거나 추천 질문 버튼을 눌러주세요.<br />
+                        2. 번역된 질문 또는 직접 입력한 영어 질문이 'ai에게 질문하기' 버튼을 누르시면 아래에 답변이 출력됩니다.
+                    </DialogContentText>
+                    <DialogActions>
+                        <Button variant='contained' onClick={() => setDialMod(false)}>닫기</Button>
+                    </DialogActions>
+                </DialogContent>
+        </Dialog><br/>
+        <Grid Containor>
+        <Grid item><QuestionAnswerIcon></QuestionAnswerIcon></Grid>
+        <Grid item xs><Typography variant="h4" component="h2">gpt api를 이용한 프로그래밍 질문 웹서비스</Typography></Grid>
+        </Grid><br/>  
         <Container>  
             <ButtonGroup>
-                <Button variant='contained' onClick={() => {}}>초기화</Button>
-                <Button variant='outlined' onClick={() => {}}>도움말 다시열기</Button>
+                <Button variant='contained' onClick={() => {window.location.reload()}}>초기화</Button>
+                <Button variant='outlined' onClick={() => {setDialMod(true)}}>도움말 다시열기</Button>
             </ButtonGroup><br/><br/>  
         </Container>
         <Container fixed> 
-            <Container>
+            <Container sx={{ border: 1, padding:2, borderColor: 'divider' }}>
             <Grid Container>
                 <Grid item xs={2}>   
                 질문 입력<br/>    
@@ -101,7 +105,7 @@ function App() {
                 추천 질문<br/><br/> 
                 <ButtonGroup variant="outlined" aria-label="outlined button group">
                 {test.map((param, index) => (
-                <Button variant="outlined" key={index} value={param} onClick={(event) => {                    
+                <Button variant="outlined" key={index} value={param||""} onClick={(event) => {                    
                         event.preventDefault();           
                         axios.post('/request',
                         {originQ: `What is the ${param}?`, originA: `What is the ${param}?`})
@@ -111,13 +115,17 @@ function App() {
                         .catch(function (error) {
                             console.log(error);
                         });
-                        setBindingQ({param})}
+                        setBindingQ(`What is the ${param}?`);
+                        
+                        
+                        }
+                        
                 }>{param}</Button>))}
                 </ButtonGroup></Grid>  
             </Grid>
             </Container>
-            <br/><hr /> 
-            <Container>    
+            <br/>
+            <Container sx={{ border: 1, padding:2, borderColor: 'divider' }}>     
             <br/>번역<br/>
             <form onSubmit={event => {
                 const LocalTransQ = event.target.transQ.value;
@@ -130,9 +138,11 @@ function App() {
                     .catch(function (error) {
                         console.log(error);
                     });
-                    setBindingQ({LocalTransQ});
-                
-                setBindingQ(LocalTransQ);    
+                    console.log("질문시 tq ="+LocalTransQ)
+                axios.get('/api/sendQ')
+                    .then(response => SetResultA(JSON.stringify(response.data.choices[0].text).replace(/\\n/gi,'\n')))
+                    .catch(error => console.log(error));     
+               
             }}>
                 <p><Input type="text" name="transQ" placeholder='영어로 직접 입력 가능' value={transQ} onChange={
                         event => {
@@ -141,14 +151,15 @@ function App() {
                 <p><Button variant='outlined' type="submit">ai에게 질문</Button></p>
             </form>
             </Container> 
-            <br/><hr />
-            <Container>    
+            <br/>
+            <Container sx={{ border: 1, padding:2, borderColor: 'divider' }}>    
             <br/>답변<br/><br/> 
-                <TextField rows="2" fullWidth multiline value={resultA} onChange={
+                <TextField fullWidth multiline value={resultA} onChange={
                 event => {
                     SetResultA(event.target.value);
                 }} >{resultA}</TextField>
             </Container>    
+        </Container>
         </Container>
         </>
     );
